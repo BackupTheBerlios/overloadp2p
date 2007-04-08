da034206be96e97d5a4f45b373ab5110601dc1a6
c-over <c-over>
2007-04-08 16:33:12 +0000

    Package containing cool global stuff

---
+++ overload/de/berlios/overloadp2p/core/shared/GlobalConstants.java
@@ -0,0 +1,8 @@
+package de.berlios.overloadp2p.core.shared;
+
+public class GlobalConstants {
+	public static final String CLIENT_NAME = "Overload";
+	public static final String CLIENT_CODE = "OVER";
+	public static final String CLIENT_VERSION = "0.1";
+	private GlobalConstants() {}
+}
